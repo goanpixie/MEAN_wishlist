b011a6bf25a9ee8e3a86e4b5ca78bed5dd4233f3
Priyanka <priyanka3007@gmail.com>
2016-09-27 14:26:06 -0400

    Got my wish list into Done v/s Pending

--- client/assets/controllers/profileController.js
+++ client/assets/controllers/profileController.js
@@ -1,49 +1,39 @@
-app.controller('profileController', ['$scope', '$location', 'userFactory', '$cookies','$routeParams',function($scope, $location, userFactory, $cookies, $routeParams) {
-$scope.newUser = {};
-$scope.wishes = {};
-$scope.selected = [];
+app.controller('profileController', ['$scope', '$location', 'userFactory', '$cookies', '$routeParams', function($scope, $location, userFactory, $cookies, $routeParams) {
+    $scope.newUser = {};
+    $scope.wishes = {};
+    $scope.selected = [];
 
 
-	if($cookies.getObject('newUser')){
-		$scope.newUser = $cookies.getObject('newUser')
-	}
-	else{
-		$location.url('/')
-	}
-
-	 // $scope.getWish = function() {
-  //       userFactory.getWish(function(data) {
-  //           $scope.wishes = data
-  //       })
-  //   };
-  //   $scope.getWish();
-
-    // $scope.toggle = function(item) {
-    //     var idx = $scope.selected.indexOf(item);
-    //         if (idx > -1) {
-    //             $scope.selected.splice(idx, 1);
-    //         } else {
-    //             $scope.selected.push(item);
-    //         }
-    //     }
-    // $scope.getWish();
+    if ($cookies.getObject('newUser')) {
+        $scope.newUser = $cookies.getObject('newUser')
+    } else {
+        $location.url('/')
+    }
 
 
-	$scope.getWishByUser = function() {
-        userFactory.getWishByUser($routeParams.id,function(data) {
-            $scope.wishes = data
+    $scope.getWishByUser = function() {
+        userFactory.getWishByUser($routeParams.id, function(data) {
+            $scope.wishes = data;
             console.log()
         })
     };
     $scope.getWishByUser();
 
-    $scope.logout = function(){
+
+    $scope.toggle = function(item) {
+        var idx = $scope.selected.indexOf(item);
+        if (idx > -1) {
+            $scope.selected.splice(idx, 1);
+        } else {
+            $scope.selected.push(item);
+        }
+    }
+    $scope.getWishByUser();
+
+
+    $scope.logout = function() {
         $cookies.remove('newUser')
         $location.url('/')
-
     }
-
-
-
 
 }])
